f66012e958f08ada665b05931b20df776bb400b0
Angus McRitchie <angus@hobbiesdirect.com.au>
2025-07-19 09:41:52 +1000

    added user resource

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,7 +26,6 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
             ->registration()
             ->login()
             ->colors([
